d5f177dabe4d45da55963c45b1652f14a3ee6ceb
Adrian Sidor <adriansidor11@wp.pl>
2020-01-19 14:14:12 +0100

    Add test class

---
+++ src/test/java/service/CarShopImplTest.java
@@ -0,0 +1,13 @@
+package service;
+
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import static org.junit.Assert.*;
+
+@RunWith(MockitoJUnitRunner.class)
+public class CarShopImplTest {
+
+
+
+}
