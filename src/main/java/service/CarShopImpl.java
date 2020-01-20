74e50d130d6c2d854cb44d09af17594e53a086c1
Adrian Sidor <adriansidor11@wp.pl>
2020-01-20 19:41:11 +0100

    Add test class

--- src/main/java/service/CarShopImpl.java
+++ src/main/java/service/CarShopImpl.java
@@ -4,6 +4,8 @@ import facade.CarDto;
 import facade.ClientDto;
 import facade.ResultDto;
 
+import model.Car;
+import remote.NotEnoughMoneyException;
 import remote.PaymentService;
 import repository.CarRepository;
 
@@ -19,6 +21,23 @@ public class CarShopImpl implements CarShop {
 
     @Override
     public ResultDto buyCar(CarDto carDto, ClientDto clientDto) {
-      // TODO implement this method
+
+        if (carDto == null || clientDto == null) {
+            throw new IllegalArgumentException("CarDto or ClientDto is null!");
+        }
+
+        Car car = carRepository.find(carDto.getId());
+
+        if (car == null) {
+            return ResultDto.carNotFound("Car not exist!");
+        }
+
+        try {
+            paymentService.pay(clientDto.getPesel(), carDto.getPrice());
+        } catch (NotEnoughMoneyException e) {
+            return ResultDto.paymentError(e.getMessage());
+        }
+
+        return ResultDto.success(carDto);
     }
 }
--- src/test/java/service/CarShopImplTest.java
+++ src/test/java/service/CarShopImplTest.java
@@ -1,13 +1,100 @@
 package service;
 
+import facade.CarDto;
+import facade.ClientDto;
+import facade.ResultDto;
+import facade.Status;
+import model.Car;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
-
-import static org.junit.Assert.*;
+import remote.NotEnoughMoneyException;
+import remote.PaymentService;
+import repository.CarRepository;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.mockito.Mockito.*;
 
 @RunWith(MockitoJUnitRunner.class)
 public class CarShopImplTest {
 
+    @Mock
+    private CarRepository carRepository;
+    @Mock
+    private PaymentService paymentService;
+    @Rule
+    public ExpectedException exception = ExpectedException.none();
+
+    private CarShop testee;
+
+    @Before
+    public void setup() {
+        testee = new CarShopImpl(carRepository, paymentService);
+    }
 
 
+    @Test
+    public void testSuccess() {
+        //given
+        CarDto carDto = new CarDto(1, 200.0);
+        ClientDto clientDto = new ClientDto("name", "lastName", "1234466778");
+        when(carRepository.find(eq(1))).thenReturn(new Car(1,"fiat", "panda", 200.0));
+
+        //when
+        ResultDto result = testee.buyCar(carDto, clientDto);
+
+        //then
+        assertEquals(Status.SUCCESS, result.getStatus());
+        assertEquals("", result.getMessage());
+        assertNotNull(result.getCarDto());
+    }
+
+    @Test
+    public void testCarNotFound() {
+        //given
+        CarDto carDto = new CarDto(1, 200.0);
+        ClientDto clientDto = new ClientDto("name", "lastName", "1234466778");
+        when(carRepository.find(eq(1))).thenReturn(null);
+
+        //when
+        ResultDto result = testee.buyCar(carDto, clientDto);
+
+        //then
+        assertEquals(Status.CAR_NOT_FOUND, result.getStatus());
+        assertEquals("Car not exist!", result.getMessage());
+        assertNull(result.getCarDto());
+    }
+
+    @Test
+    public void testPaymentError() throws NotEnoughMoneyException {
+        //given
+        CarDto carDto = new CarDto(1, 200.0);
+        ClientDto clientDto = new ClientDto("name", "lastName", "1234466778");
+        when(carRepository.find(eq(1))).thenReturn(new Car(1,"fiat", "panda", 200.0));
+        doThrow(NotEnoughMoneyException.class).when(paymentService).pay(anyString(), anyDouble());
+
+        //when
+        ResultDto result = testee.buyCar(carDto, clientDto);
+
+        //then
+        assertEquals(Status.PAYMENT_ERROR, result.getStatus());
+        assertNull(result.getCarDto());
+    }
+
+    @Test
+    public void testBuyCarWhenExceptionThrown() {
+        //given
+        CarDto carDto = new CarDto(1, 200.0);
+        ClientDto clientDto = null;
+        exception.expect(IllegalArgumentException.class);
+        exception.expectMessage("CarDto or ClientDto is null!");
+
+        //when
+        ResultDto result = testee.buyCar(carDto, clientDto);
+    }
 }
